0c915e3ffe59455e8486b56be912f633aba4dcaf
JegoRamos <jego.ramos@blackpearl.technology>
2019-12-18 09:46:07 +0800

    refactored mongoose connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose')
 const DATABASE_URL = process.env.DATABASE_URL
 mongoose.set('useFindAndModify', false)
 
-mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-})
-  // .then(() => console.log('MongoDB running'))
-  .then(() => { })
-  .catch(error => console.error(error))
+module.exports.connect = () =>
+  mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
